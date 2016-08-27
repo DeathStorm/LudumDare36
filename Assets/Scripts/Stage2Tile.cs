3ff8e4f54e80ca0bb7edf5140866f9b7e81e0567
DeathStorm <Death_Storm@web.de>
2016-08-27 23:45:29 +0200

    d

---
+++ Assets/Scripts/Stage2Tile.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+using FORMS = ENUMS.FORMS;
+using MATERIALGROUPS = ENUMS.MATERIALGROUPS;
+using MATERIALS = ENUMS.MATERIALS;
+
+public class Stage2Tile : MonoBehaviour
+{
+
+    public Stage2_GridManager gridManager;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+   
+}
--- Assets/Stage2_GridManager.cs
+++ Assets/Stage2_GridManager.cs
@@ -7,13 +7,37 @@ using MATERIALS = ENUMS.MATERIALS;
 
 public class Stage2_GridManager : MonoBehaviour {
 
+    public GameObject prefabTileStage2;
+
     public int gridWidth = 13;
     public int gridHeight = 13;
     public float tileSize = 0.645f;
 
+    public GameObject[,] stage2TileObjects;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+    {
+        stage2TileObjects = new GameObject[gridWidth, gridHeight];
+
+        for (int width = 0; width < gridWidth; width++)
+        {
+            for (int height = 0; height < gridHeight; height++)
+            {
+                // if ((FORMS)bluePrint_Dagger[0, width, height] != FORMS.Empty)
+                {
+                    GameObject tmpObject = (GameObject)Instantiate(prefabTileStage2, new Vector3(width * tileSize, height * tileSize, 0), prefabTileStage2.transform.rotation, transform);
+                    tmpObject.name = "Stage2Tile_W" + width.ToString() + "_H" + height.ToString();
+
+                    Stage2Tile stage2Tile = tmpObject.GetComponent<Stage2Tile>();
+                    stage2Tile.gridManager = this;
+
+                    stage2TileObjects[width, height] = tmpObject;
+                }
+
+            }
+        }
+
 	}
 	
 	// Update is called once per frame
