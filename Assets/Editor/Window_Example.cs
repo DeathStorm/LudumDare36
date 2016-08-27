92914a7f2a66a0f44068609e88ae02190961b742
DeathStorm <Death_Storm@web.de>
2016-08-27 13:17:34 +0200

    new init

---
+++ Assets/Editor/Window_Example.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Window_Example : EditorWindow{
+        
+    //
+    // ~~~~~~ ShowWindow ~~~~~~
+    //
+    [MenuItem("TDP/Window_Example", false, 10)]
+    public static void ShowWindow()
+    {
+
+        EditorWindow.GetWindow(typeof(Window_Example));
+
+    } // END ShowWindow
+
+
+    void OnGUI()
+    {
+       
+    } // END OnGui
+
+
+
+    
+} // END Class
